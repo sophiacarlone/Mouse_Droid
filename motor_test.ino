4193baf6101b3bef61e540e48a517c61cb047249
Sophia Carlone <71948392+sophiacarlone@users.noreply.github.com>
2024-06-24 15:52:36 -0400

    Update motor_test.ino

--- motor_test.ino
+++ motor_test.ino
@@ -3,8 +3,8 @@
 Servo myservo1;
 Servo myservo2;
 
-int motor1 = 9;
-int motor2 = 8;
+int motor1 = 9; //right (180-backwards, 0 -forwards)
+int motor2 = 8; //left (0-backwards, 180-forwards)
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,6 +15,13 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   myservo1.write(0); //no moment
-  myservo2.write(180); //0 is forwards, 180 is backwards
-  delay(100000);
+  myservo2.write(0); //0 is forwards, 180 is backwards
+  delay(1000);
 }
+
+/*
+ * right: servo 1 - 180, servo 2 - 180
+ * forward: servo 1 - 0, servo 2 - 180
+ * backward: servo 1 - 180, servo 2 - 0
+ * left: servo 1 - 0, servo 2 - 0
+ */
